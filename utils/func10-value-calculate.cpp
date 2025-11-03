4b8aea28b43bc5daee3713ef39e3d77c3484444d
liahrothstein <liahrothstein@gmail.com>
2025-11-03 12:10:53 +0300

    fix: change exponenta function

--- utils/func10-value-calculate.cpp
+++ utils/func10-value-calculate.cpp
@@ -1,5 +1,5 @@
 #include <cmath>
 
 double func10ValueCalculate(double x) {
-    return ((pow(M_E, pow(sin(x), 3)) + log(x + 1)) / sqrt(x));
+    return (exp(pow(sin(x), 3)) + log(x + 1)) / sqrt(x);
 }
--- utils/func5-value-calculate.cpp
+++ utils/func5-value-calculate.cpp
@@ -1,5 +1,5 @@
 #include <cmath>
 
 double func5ValueCalculate(double x) {
-    return (log(x + 0.5) + (pow(M_E, x) - pow(M_E, -x)));
+    return (log(x + 0.5) + (exp(x) - exp(-x)));
 }
